cfc738bcda4528b40cbdf3b61175f6bf9601d89d
大嶋広也 <ooshimahiroya@ooshimahironaris-macbook-air.local>
2017-03-04 15:07:18 +0900

    Initial Commit

---
+++ TouchEigo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchEigo
+//
+//  Created by 大嶋 広也 on 2017/03/04.
+//  Copyright © 2017年 大嶋 広也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
